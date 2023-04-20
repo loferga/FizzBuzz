b7b01d3c3713093a7f187715c2cb19da833a4b7c
loferga <laforgemateo@gmail.com>
2023-04-20 16:56:22 +0200

    test4 refactor
    architecture émergente

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,14 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	
+	private static final int[] MULTIPLES = {3, 5};
+	private static final String[] RETOURS_ASSOCIES = {"fizz", "buzz"};
+	
 	public String fizzbuzz(int n) {
 		StringBuilder str = new StringBuilder();
-		if (n%3==0) str.append("fizz");
-		if (n%5==0) str.append("buzz");
+		for (int i = 0; i<MULTIPLES.length; i++)
+			if (n%MULTIPLES[i] == 0)
+				str.append(RETOURS_ASSOCIES[i]);
 		if (str.length() == 0) str.append(Integer.toString(n));
 		return str.toString();
 	}

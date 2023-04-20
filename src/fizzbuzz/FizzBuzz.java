fd6f0d283db713059a618eb521db66f992243ee5
loferga <laforgemateo@gmail.com>
2023-04-20 16:40:35 +0200

    test3

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,8 @@ package fizzbuzz;
 public class FizzBuzz {
 	
 	public static String fizzbuzz(int n) {
+		if (n%3 == 0) return "fizz";
+		if (n%5 == 0) return "buzz";
 		return Integer.toString(n);
 	}
 	
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -20,5 +20,29 @@ class FizzBuzzTest {
 	void fizz_deux() {
 		assertEquals(FizzBuzz.fizzbuzz(2), "2");
 	}
+	
+	@Test
+	void fizz_trois() {
+		assertEquals(FizzBuzz.fizzbuzz(3), "fizz");
+	}
+	
+	@Test
+	void fizz_quelques_valeurs() {
+		assertEquals(FizzBuzz.fizzbuzz(4), "4");
+		assertEquals(FizzBuzz.fizzbuzz(7), "7");
+	}
+	
+	@Test
+	void fizz_cinq() {
+		assertEquals(FizzBuzz.fizzbuzz(5), "buzz");
+	}
+	
+	@Test
+	void fizz_multiples() {
+		assertEquals(FizzBuzz.fizzbuzz(6), "fizz");
+		assertEquals(FizzBuzz.fizzbuzz(10), "buzz");
+		assertEquals(FizzBuzz.fizzbuzz(9), "fizz");
+		assertEquals(FizzBuzz.fizzbuzz(400), "buzz");
+	}
 
 }
